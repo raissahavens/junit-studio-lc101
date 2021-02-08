2583d7bbcc5309289f3b2af0365216f3a1cfb51e
Raissa Havens <raissahavens@gmail.com>
2021-02-08 20:14:00 -0200

    Changes

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,39 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void BracketsFollowedByStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void BracketsWithStringPartiallyOutsideReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void BracketsWithStringsInsideReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void OneBracketWithStringsReturnsTrue() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void OneBracketOnlyReturnsTrue() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void OppositeBracketsReturnsTrue() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
 
 }
